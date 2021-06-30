571e546c62bb3dcec236026129cf28264b9a9442
Mads <mandi.awale.com>
2021-06-30 15:51:00 +1000

    added onclick handler to button component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    console.log('toggled');
   }
-
 }
